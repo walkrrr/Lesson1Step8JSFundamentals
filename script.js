a322c82977b5492c64d1bdac5979727a16db929e
RWR <rwr8106@gmail.com>
2022-09-16 16:08:37 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Write your JavaScript below this line
+
+// Lesson 1, Step 8
+
+// exercise 1
+var numberOfHorses = 89;
+console.log(numberOfHorses);
+
+// exercise 2
+var favoriteFlower = "rose";
+console.log("My favorite flower is a " + favoriteFlower + ".");
+
+// exercise 3
+var city = "Long Island City";
